2fff1c746048999b56eaa179f2e0c22b4219c5d2
riccmar <riccardomarconi01@gmail.com>
2026-01-06 16:39:54 +0100

    Aggiunto file per Loss functions e integrazione nel training di ResNet

--- scripts/baseline/resnet/resnet_train.py
+++ scripts/baseline/resnet/resnet_train.py
@@ -16,32 +16,7 @@ if project_root not in sys.path:
 
 from dataset.dataset_baseline import RotationResNetDataset
 from models.models_baseline import RotationResNet
-
-class QuaternionLoss(nn.Module):
-    def __init__(self):
-        super(QuaternionLoss, self).__init__()
-
-    def forward(self, pred_q, target_q):
-        """
-        pred_q: [batch_size, 4] (Network output)
-        target_q: [batch_size, 4] (Ground Truth from dataset)
-        """
-        # Normalize the predictions
-        pred_q = F.normalize(pred_q, p=2, dim=1)
-
-        # Normalize target
-        target_q = F.normalize(target_q, p=2, dim=1)
-
-        # Calculate Dot Product
-        dot_product = torch.sum(pred_q * target_q, dim=1)
-
-        # Handle Double Cover
-        # If dot_product is 1.0/-1.0 -> Perfect match
-        # If dot_product is 0.0 -> Terrible match (90 degrees off)
-        loss = 1.0 - torch.abs(dot_product)
-
-        # Return mean over the batch
-        return loss.mean()
+from models.losses_baseline import QuaternionLoss
 
 def resnet_training(device='cpu', epochs=50, batch_size=64, lr=0.0001):
     """
